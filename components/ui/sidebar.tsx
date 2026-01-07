c87c15cb33c2cb7f47f59b33bc93f1c1707d9fb4
proxy <muhammadyusufa2111@gmail.com>
2026-01-07 20:15:07 +0500

    added padding top for sidebar in mini app

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -335,7 +335,7 @@ function SidebarInput({
 function SidebarHeader({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
-      data-slot="sidebar-header"
+      data-slot="sidebar-header tg-safe-top"
       data-sidebar="header"
       className={cn("flex flex-col gap-2 p-2", className)}
       {...props}
